fc2cc4b6edaf9172d438eeceb1644cca0c00e623
Stupidbird0 <russo.franco369@gmail.com>
2026-03-27 16:16:42 +0100

    Delete main directory

--- main/java/com/weather/spring/service/WeatherService.java
+++
@@ -1,7 +0,0 @@
-package com.weather.spring.service;
-
-import com.weather.spring.model.WeatherData;
-
-public interface WeatherService {
-    WeatherData getWeatherByCity(String city) throws Exception;
-}
